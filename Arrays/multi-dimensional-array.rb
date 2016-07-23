88e1670613bc1d228b85b5555580842f38916a8b
Jai Srikar <gruprog@gmail.com>
2016-07-23 10:51:40 -0400

    multi-d array

---
+++ Arrays/multi-dimensional-array.rb
@@ -0,0 +1,13 @@
+array0 = [ ['a','b','c'],
+	[1, 2, 3],
+	["Welcome", "Ruby"]
+]
+
+p(array0)
+
+multid_array = Array.new(2)
+
+multid_array[0] = Array.new(5, 1)
+multid_array[1] = Array.new(2, 'a')
+
+p(multid_array)
