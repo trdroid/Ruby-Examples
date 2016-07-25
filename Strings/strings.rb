5e6b08365a49d365225d147bc0aec2d50ef0fb68
Jai Srikar <gruprog@gmail.com>
2016-07-25 10:54:03 -0400

    general strings example

---
+++ Strings/strings.rb
@@ -0,0 +1,20 @@
+puts("Welcome")
+puts('Ruby')
+
+print('Welcome')
+print('Ruby')
+
+print('\n')
+print("\n")
+
+temp = 10
+
+puts('#{temp}')
+puts("#{temp}")
+
+puts(%Q/Double quoted: #{temp}/)
+puts(%Q(Double quoted: #{temp}))
+puts(%Q~Double quoted: #{temp}~)
+
+puts(%/Double quoted: #{temp}/)
+puts(%q/Single quoted: #{temp}/)
