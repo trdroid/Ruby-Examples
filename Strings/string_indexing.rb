f9498c23b787f353146283e8a8042ba958c2a98a
Jai Srikar <gruprog@gmail.com>
2016-07-26 10:54:36 -0400

    string indexing example

---
+++ Strings/string_indexing.rb
@@ -0,0 +1,11 @@
+str = "Welcome Ruby"
+
+puts(str[0])
+
+# [startIndex, numberOfCharsToRead]
+puts(str[0, 1])
+puts(str[-1, 1])
+
+# [startIndex, endIndexInclusive]
+puts(str[0..1])
+puts(str[-4..-1])
